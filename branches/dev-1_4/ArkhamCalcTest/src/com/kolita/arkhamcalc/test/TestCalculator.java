2c9560f108b4c18d5df922056f5e6ad21381082c
matthewjcole@gmail.com <matthewjcole@gmail.com@caf8d270-429d-f773-d715-b115897df9aa>
2011-11-05 16:40:56 +0000

    moving TestCalculator to ArkhamCalcTest project

--- branches/dev-1_4/ArkhamCalcTest/src/com/kolita/arkhamcalc/test/TestCalculator.java
+++ branches/dev-1_4/ArkhamCalcTest/src/com/kolita/arkhamcalc/test/TestCalculator.java
@@ -15,9 +15,11 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.*/
 
-package com.kolita.arkhamcalc;
+package com.kolita.arkhamcalc.test;
 
 import java.util.Random;
+
+import com.kolita.arkhamcalc.Calculator;
 
 import junit.framework.TestCase;
 
