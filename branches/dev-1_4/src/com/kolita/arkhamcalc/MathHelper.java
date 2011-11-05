ce8ef36ac10a21d1ddc1e9f3ca3f36ce50e1bc44
matthewjcole@gmail.com <matthewjcole@gmail.com@caf8d270-429d-f773-d715-b115897df9aa>
2011-11-05 15:45:20 +0000

    branching dev 1.4

---
+++ branches/dev-1_4/src/com/kolita/arkhamcalc/MathHelper.java
@@ -0,0 +1,35 @@
+/*ArkhamCalc
+Copyright (C) 2011  Matthew Cole
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.*/
+
+package com.kolita.arkhamcalc;
+
+public class MathHelper
+{
+	/**
+	 * From n, choose r.  See http://en.wikipedia.org/wiki/Combination
+	 */
+	public static long nCr(int n, int r)
+	{
+		return factorial(n) / (factorial(n - r) * factorial(r));
+	}
+	
+	private static long factorial(int n)
+	{
+		if (n <= 1) return 1;
+		return n * factorial(n - 1);
+	}
+}
